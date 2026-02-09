1ae21f404942609cd0390060a2b9b764d0e327df
stanley-agwu <stanleyagwu2016@gmail.com>
2026-02-09 22:18:36 +0100

    feat(Algorithms): Tree: Height of a Binary Tree - Python

--- Dynamic Programming/Python Algorithms/More Algorithms/height_binary_tree.py
+++ Dynamic Programming/Python Algorithms/More Algorithms/height_binary_tree.py
@@ -2,7 +2,7 @@
 
 """
 The height of a binary tree is the number of edges between the tree's 
-root and its furthest leaf. 
+root and its furtherest leaf.
 
 Write a function that returns a Tree's height, given the root node:
 root: a reference to the root of a binary tree.
@@ -15,28 +15,31 @@ Note: In a binary search tree, all nodes on the left branch of a node
 
 # Key Idea
 """
-Since we are counting edges, the number of edges between n nodes is n - 1 edges.
-
-Thus, in base cases instead of returning 0, we return -1
+1. Since we are counting edges, the number of edges between n nodes is n - 1 edges.
+2. Thus, in base cases instead of returning 0, we return -1
 """
 
 class Node:
-      def __init__(self,info): 
+      def __init__(self, info): 
           self.info = info  
           self.left = None  
           self.right = None 
 # this is a node of the tree , which contains info as data, left , right
 
 # Recursive solution
-def height(root):
+def height(root: Node | None) -> int:
     if not root:
         return -1
     return 1 + max(height(root.left), height(root.right))
+
+# O(n) - Time complexity -> Every node visited atmost once
+# O(n) worst case - Space complexity -> O(log n) in avg or best case 
+#   (Corresponds to height of tree)
     
 # Iterative solution
 from collections import deque
 
-def height(root):
+def height(root: Node | None) -> int:
     if root is None:
         return -1
 
